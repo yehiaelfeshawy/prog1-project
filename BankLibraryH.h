ebbb10e4947b2c49024c5951e383245f05c792f0
adhammostafaa <adhammostafa.amr@gmail.com>
2025-12-26 11:52:35 +0200

    Update BankLibraryH.h

--- BankLibraryH.h
+++ BankLibraryH.h
@@ -1,1 +1,41 @@
+#ifndef BANKLIBRARYH_H_INCLUDED
+#define BANKLIBRARYH_H_INCLUDED
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct
+{int month;
+int year;
+} Date;
+
+
+typedef struct
+{
+long long accountNumber;
+char name[100];
+char email[100];
+double balance;
+char mobile[20];
+Date opened;
+char status[20];
+} Account;
+
+int login(void);
+
+int loadAccounts(Account acc[], int *count);
+
+int query(Account acc[], int);
+int AdvancedSearch(Account acc[], int);
+int addAccount(Account acc[], int *count, int max_acc);
+
+int deleteAccount(Account acc[], int *count);
+int modifyAccount(Account acc[], int);
+
+
+void runSystem(void);
+
+#endif // BANKLIBRARYH_H_INCLUDED
 
